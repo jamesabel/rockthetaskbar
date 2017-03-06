dbb3cb8d16ec3323066e5fa07d38e9a19d9407ba
james abel <j@abel.co>
2017-03-05 18:51:32 -0800

    add gridlines and CPU core/thread info

--- rockthetaskbar/gui.py
+++ rockthetaskbar/gui.py
@@ -2,7 +2,7 @@
 import sys
 import copy
 
-
+import psutil
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QGridLayout, QLabel, QSystemTrayIcon, QMenu, QDialog, QApplication
 
@@ -13,6 +13,7 @@ import rockthetaskbar.cpumonitor
 # see: http://stackoverflow.com/questions/29992771/combining-pyqt-and-matplotlib
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+from matplotlib.ticker import MultipleLocator
 
 
 class CPUDialog(QDialog):
@@ -27,11 +28,13 @@ class CPUDialog(QDialog):
 
         # do the plotting
         ax = fig.add_subplot(1, 1, 1)  # 1x1 grid, first subplot
-        ax.set_title('CPU Usage Histogram')
+        ax.set_title('CPU Usage Histogram (%s Cores/%s Threads)' % (psutil.cpu_count(False), psutil.cpu_count(True)))
         ax.set_ylabel('Count')
         ax.set_xlabel('CPU %')
+        ax.grid(True)
         xs = range(0, 101)
         ax.plot(xs, [cpu_histogram[x] for x in xs])
+        ax.xaxis.set_major_locator(MultipleLocator(10.))
 
         self.show()
 
