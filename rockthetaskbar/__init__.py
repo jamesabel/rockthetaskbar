9c15ded2f104dc2914d37645f196e826d8b74a0e
james abel <j@abel.co>
2017-03-11 12:09:48 -0800

    workaround to put spaces in author name for nsis

--- rockthetaskbar/__init__.py
+++ rockthetaskbar/__init__.py
@@ -1,6 +1,5 @@
 
 __application_name__ = 'rockthetaskbar'
-__author__ = 'James Abel'
+__author__ = '"James Abel"'  # kind of a workaround for nsis - osnap will be enhanced to tolerate regular spaces w/o double quoting out like this
 __url__ = 'https://github.com/jamesabel/rockthetaskbar'
 __python_version__ = '3.6.0'
-
