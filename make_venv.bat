5de02727ae4d95428706498825000a3306084956
james abel <j@abel.co>
2017-03-13 11:47:26 -0700

    windows stays on 3.5

--- make_venv.bat
+++ make_venv.bat
@@ -1,4 +1,5 @@
-\Users\james\AppData\Local\Programs\Python\Python36\python.exe -m venv --clear venv
+REM stay on 3.5 until pip bug in embedded 3.6 fixed
+\Users\james\AppData\Local\Programs\Python\Python35\python.exe -m venv --clear venv
 venv\Scripts\pip3 install -U pip
 venv\Scripts\pip3 install -U setuptools
 venv\Scripts\pip3 install -r requirements.txt
