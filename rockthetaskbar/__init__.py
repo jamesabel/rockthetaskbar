1dd70be9cb55fc0b19c6934ddc8411039a0a3fce
james abel <j@abel.co>
2017-03-12 22:21:26 -0700

    add version

--- rockthetaskbar/__init__.py
+++ rockthetaskbar/__init__.py
@@ -1,5 +1,6 @@
 
 __application_name__ = 'rockthetaskbar'
+__version__ = '0.0.0'
 __author__ = '"James Abel"'  # kind of a workaround for nsis - osnap will be enhanced to tolerate regular spaces w/o double quoting out like this
 __url__ = 'https://github.com/jamesabel/rockthetaskbar'
 __python_version__ = '3.6.0'
