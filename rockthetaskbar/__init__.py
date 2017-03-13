aba832fa55ef0a0314e947290b4957b8ea1967db
james abel <j@abel.co>
2017-03-13 11:25:02 -0700

    conforming url

--- rockthetaskbar/__init__.py
+++ rockthetaskbar/__init__.py
@@ -2,5 +2,5 @@
 __application_name__ = 'rockthetaskbar'
 __version__ = '0.0.0'
 __author__ = '"James Abel"'  # kind of a workaround for nsis - osnap will be enhanced to tolerate regular spaces w/o double quoting out like this
-__url__ = 'https://github.com/jamesabel/rockthetaskbar'
+__url__ = 'www.abel.co'  # must be a simple x.y.z
 __python_version__ = '3.6.0'
